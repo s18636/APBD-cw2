6fbb200b988e1ee358ec27c0a98fddbf5400724a
kubas <s18636@pjwstk.edu.pl>
2020-03-24 12:10:40 +0100

    initial commit with arguments validation

---
+++ APBD-cw2/APBD-cw2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace APBD_cw2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string CSVPath = "data/dane.csv";
+            string outputPath = "data/result.xml";
+            string dataType = "xml";
+
+            if (args.Length == 3)
+            {
+                CSVPath = args[0];
+                outputPath = args[1];
+                dataType = args[2];
+
+            }
+            else if (args.Length < 3)
+            {
+                Regex firstArg = new Regex("[\\w\\/]*\\.csv");
+                Regex secondArg = new Regex("[\\w\\/]*\\.\\w*");
+                Regex thirdArg = new Regex("^[^\\/]$");
+
+                foreach (var arg in args)
+                {
+                    if (firstArg.Matches(arg).Count > 0)
+                    {
+                        CSVPath = arg;
+                        break;
+                    }
+                }
+                foreach (var arg in args)
+                {
+                    if (secondArg.Matches(arg).Count > 0)
+                    {
+                        outputPath = arg;
+                        break;
+                    }
+                }
+                foreach (var arg in args)
+                {
+                    if (thirdArg.Matches(arg).Count > 0)
+                    {
+                        dataType = arg;
+                        break;
+                    }
+                }
+            }
+
+            Console.Write($"input: {CSVPath} output: {outputPath} type: {dataType}");
+
+        }
+
+        public static void WriteToLog(string msg)
+        {
+
+        }
+    }
+}
